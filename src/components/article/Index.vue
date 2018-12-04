86f63985e4f89314e6ae6429c999f69aff906a82
duc <1025434218@qq.com>
2018-12-04 10:29:16 +0800

    preview

--- src/components/article/Index.vue
+++ src/components/article/Index.vue
@@ -13,7 +13,7 @@
           placeholder="搜索文章 elasticsearch..."
         />
       </i-col>
-     <i-col :span="6">
+      <i-col :span="6">
         <Button
           type="dashed"
           @click="createArticle"
@@ -68,18 +68,36 @@
       </i-col>
     </Row>
     <Modal
-        v-if="previewArticle !== null"
-        width="80"
-        v-model="modalVisible"
-        title="title"
-       >
-        <p v-html="previewArticle.content" v-highlight></p>
+      v-if="previewArticle !== null"
+      width="80"
+      v-model="modalVisible"
+    >
+    <div>
+      <div
+        slot="header"
+        style="display: flex;align-items: center;"
+      >
+        <img style="width:30px;height:30px;border-radius:50%;margin-right: 20px;" :src="previewArticle.author.avatar" alt="author image">
+        <span v-text="previewArticle.author.name" style="font-size: 14px;"></span>
+      </div>
+        <h1 style="text-align:center;margin-bottom: 20px;">{{previewArticle.title}}</h1>
+        <Divider dashed />
+      </div>
+      <p
+        v-html="previewArticle.content"
+        v-highlight
+      ></p>
     </Modal>
   </div>
 </template>
 <script>
 import _ from 'lodash'
-import { index, deleteArticle, adminElasticSearchArticle, show } from '@/api/article'
+import {
+  index,
+  deleteArticle,
+  adminElasticSearchArticle,
+  show
+} from '@/api/article'
 
 export default {
   props: ['all'],
@@ -212,12 +230,11 @@ export default {
   },
 
   methods: {
-    ok () {
-
+    title () {
+      return this.previewArticle.author.name + this.previewArticle.title
     },
-    cancel () {
-
-    },
+    ok () {},
+    cancel () {},
     preview (id) {
       console.log('preview: ' + id)
       show({ id }).then(res => {
@@ -250,14 +267,16 @@ export default {
         content: '确认要删除这篇文章吗？',
         loading: true,
         onOk: () => {
-          deleteArticle(1).then(res => {
-            this.$Modal.remove()
-            this.$Message.success('删除成功')
-            this.dataSet.data = _.reject(this.dataSet.data, { id: id })
-          }).catch(e => {
-            this.$Modal.remove()
-            this.$Message.error(e.data.error.message)
-          })
+          deleteArticle(1)
+            .then(res => {
+              this.$Modal.remove()
+              this.$Message.success('删除成功')
+              this.dataSet.data = _.reject(this.dataSet.data, { id: id })
+            })
+            .catch(e => {
+              this.$Modal.remove()
+              this.$Message.error(e.data.error.message)
+            })
         }
       })
     },
@@ -280,7 +299,10 @@ export default {
     },
 
     async elasticSearchArticle () {
-      const data = await adminElasticSearchArticle({ query: this.search, all: this.all ? 1 : 0 })
+      const data = await adminElasticSearchArticle({
+        query: this.search,
+        all: this.all ? 1 : 0
+      })
       this.dataSet = data
     },
 
